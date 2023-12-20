199ebcb2869012b51603061670c32108bb5097dc
Adil GAHRAMANLI <adil.gahramanli@Digital-Grenoble.local>
2023-12-20 16:54:32 +0100

    création classe

---
+++ Helloworld/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Message
+	private string helloMessage = "hello !!"
+
+	public Message()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+	public string HelloMessage()
+{
+	get => HelloMessage;
+}
+
+
+}
--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -35,6 +35,8 @@ namespace Helloworld
 
             messager.dateMessager();
 
+           
+
             Console.ReadLine();
 
         }
