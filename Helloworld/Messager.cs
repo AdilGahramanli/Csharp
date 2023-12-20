cc39fec386b9bb8597e1445913e4000255ff8a78
Adil GAHRAMANLI <adil.gahramanli@Digital-Grenoble.local>
2023-12-20 15:40:23 +0100

    premier commit 1.1 terminé

---
+++ Helloworld/Messager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class Messager
+    {
+        public string[] intervalDays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+        public string[] intervalWeekEnd = { "Saturday", "Sunday" };
+        public string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        public void dateMessager()
+        {
+
+
+            int hour = DateTime.Now.Hour;
+
+            if (hour >= 9 && hour <= 13 && intervalDays.Contains(DateTime.Now.DayOfWeek.ToString()))
+            {
+
+                Console.WriteLine("Bonjour " + userName);
+
+            }
+
+            if (hour >= 13 && hour <= 18 && intervalDays.Contains(DateTime.Now.DayOfWeek.ToString()))
+            {
+
+                Console.WriteLine("Bon après-midi " + userName);
+
+            }
+
+            if ((hour >= 18 || hour <= 9 && intervalDays.Contains(DateTime.Now.DayOfWeek.ToString()) && DateTime.Now.DayOfWeek.ToString() != "Friday"))
+            {
+
+                Console.WriteLine("Bonsoir " + userName);
+
+            }
+
+            //Le code ci-dessous est monstrueusement troooooooooop lonnnnnnnnnnng !!!!! (et ci-dessus c'est pas mieux!!)
+            if (((hour >= 18 || hour <= 9) && (DateTime.Now.DayOfWeek.ToString() != "Friday" || intervalWeekEnd.Contains(DateTime.Now.DayOfWeek.ToString()))))
+            {
+
+                Console.WriteLine("Bon week-end");
+
+            }
+
+        }
+    }
+}
---
+++ Helloworld/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Helloworld
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Messager messager = new Messager();
+            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine("Hello " + messager.userName);
+
+            
+            
+            string dateDayName = DateTime.Now.DayOfWeek.ToString();
+
+
+            for (int i = 0; i < messager.intervalDays.Length; i++)
+            {
+                if (dateDayName == messager.intervalDays[i])
+                {
+
+                    Console.WriteLine("Nous sommes Mercredi");
+
+                }
+            }
+
+            messager.dateMessager();
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
+
