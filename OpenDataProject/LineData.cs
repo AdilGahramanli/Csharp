891589c7ae59a676a3904ef458c3ab2e23fafe26
Adil GAHRAMANLI <adil.gahramanli@Digital-Grenoble.local>
2024-01-02 14:46:16 +0100

    Premier commit pour OpenData, WebRequest Ok, Serialization début

---
+++ OpenDataProject/LineData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDataProject
+{
+    internal class LineData
+    {
+        /* "id":"SEM:1696","name":"Grenoble, Chavant","lon":5.73233,"lat":45.18502,"zone":"SEM_GENCHAVANT","lines":["SEM:13","SEM:C4" */
+        public int id;
+        public string name;
+        public double lon;
+        public double lat;
+        public string zone;
+        public string lines;
+        public LineData(int id, string name, double lon, double lat, string zone, string lines)
+        {
+            this.id = id;
+            this.name = name;
+            this.lon = lon;
+            this.lat = lat;
+            this.zone = zone;
+            this.lines = lines;
+        }
+    }
+}
---
+++ OpenDataProject/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Net;
+using System.Reflection.PortableExecutable;
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+using OpenDataProject;
+ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 |
+SecurityProtocolType.Tls;
+Console.WriteLine("Hello, World!");
+
+OpenDataRequest requestApi = new OpenDataRequest();
+
+Console.WriteLine(requestApi.affichageUrl());
+
+Console.WriteLine(requestApi.requestUrl());
+Stream dataStream = requestApi.requestUrl().GetResponseStream();
+StreamReader reader = new StreamReader(dataStream);
+string responseFromServer = reader.ReadToEnd();
+Console.WriteLine(responseFromServer);
+reader.Close();
+dataStream.Close();
+
+
+
+
+
