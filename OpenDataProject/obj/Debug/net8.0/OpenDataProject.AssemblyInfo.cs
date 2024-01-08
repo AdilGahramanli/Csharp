7149f895714693c3956d8b0e74c3c7a0de93ddc1
Adil GAHRAMANLI <adil.gahramanli@Digital-Grenoble.local>
2024-01-08 16:53:21 +0100

    Wpf modifs

--- OpenDataProject/obj/Debug/net8.0/OpenDataProject.AssemblyInfo.cs
+++ OpenDataProject/obj/Debug/net8.0/OpenDataProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenDataProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14343fd7650d0f341850bdda0ca0310d96821793")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e41fb547a284473202522ba3ea437003922dec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenDataProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenDataProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
